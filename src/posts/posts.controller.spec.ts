25ba2f27449eb22e20a2dce568c34a2865359ee3
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-09-08 14:49:23 +0300

    fix: controller spec

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
+  let service: PostsService;
+
+  const mockPostsService = {
+    createPost: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    updatePost: jest.fn(),
+    deletePost: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [
+        {
+          provide: PostsService,
+          useValue: mockPostsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
+    service = module.get<PostsService>(PostsService);
   });
 
   it('should be defined', () => {
